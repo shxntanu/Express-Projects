db303c3b78f1fc3f96f718311d02bab43029cbc8
Shantanu <shantanuwable2003@gmail.com>
2023-08-20 09:56:55 +0530

    Task Manager Init

---
+++ task-manager/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks");
+connectDB = require("./db/connect");
+require("dotenv").config();
+
+// middleware
+app.use(express.static("./public"));
+app.use(express.json());
+
+const port = 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening on port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
+
+app.use("/api/v1/tasks", tasks);
---
+++ task-manager/db/connect.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const connectDB = (url) => {
+  mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  });
+};
+
+module.exports = connectDB;
